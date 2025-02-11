65ba41d10bb8235859f836bb809d17cafdb5ba7a
Stung16 <kieuduytung3@gmail.com>
2025-02-11 18:13:40 +0700

    fix api

--- src/config.js
+++ src/config.js
@@ -142,282 +142,230 @@ class ZingMp3Api {
 
   // getDetailPlaylist
   getDetailPlaylist(playlistId) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/page/get/playlist", {
-          id: playlistId,
-          sig: this.hashParam("/api/v2/page/get/playlist", playlistId),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/page/get/playlist", {
+        id: playlistId,
+        sig: this.hashParam("/api/v2/page/get/playlist", playlistId),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getHome
   getHome() {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/page/get/home", {
-          page: 1,
-          segmentId: "-1",
-          count: "30",
-          sig: this.hashParamHome("/api/v2/page/get/home"),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/page/get/home", {
+        page: 1,
+        segmentId: "-1",
+        count: "30",
+        sig: this.hashParamHome("/api/v2/page/get/home"),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getTop100
   getTop100() {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/page/get/top-100", {
-          sig: this.hashParamNoId("/api/v2/page/get/top-100"),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/page/get/top-100", {
+        sig: this.hashParamNoId("/api/v2/page/get/top-100"),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getChartHome
   getChartHome() {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/page/get/chart-home", {
-          sig: this.hashParamNoId("/api/v2/page/get/chart-home"),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/page/get/chart-home", {
+        sig: this.hashParamNoId("/api/v2/page/get/chart-home"),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getNewReleaseChart
   getNewReleaseChart() {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/page/get/newrelease-chart", {
-          sig: this.hashParamNoId("/api/v2/page/get/newrelease-chart"),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/page/get/newrelease-chart", {
+        sig: this.hashParamNoId("/api/v2/page/get/newrelease-chart"),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getInfoSong
   getInfoSong(songId) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/song/get/info", {
-          id: songId,
-          sig: this.hashParam("/api/v2/song/get/info", songId),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/song/get/info", {
+        id: songId,
+        sig: this.hashParam("/api/v2/song/get/info", songId),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   getListArtistSong(artistId, page, count) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/song/get/list", {
-          id: artistId,
-          type: "artist",
-          page: page,
-          count: count,
-          sort: "new",
-          sectionId: "aSong",
-          sig: this.hashListMV(
-            "/api/v2/song/get/list",
-            artistId,
-            "artist",
-            page,
-            count
-          ),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/song/get/list", {
+        id: artistId,
+        type: "artist",
+        page: page,
+        count: count,
+        sort: "new",
+        sectionId: "aSong",
+        sig: this.hashListMV(
+          "/api/v2/song/get/list",
+          artistId,
+          "artist",
+          page,
+          count
+        ),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getArtist
   getArtist(name) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/page/get/artist", {
-          alias: name,
-          sig: this.hashParamNoId("/api/v2/page/get/artist"),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/page/get/artist", {
+        alias: name,
+        sig: this.hashParamNoId("/api/v2/page/get/artist"),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getLyric
   getLyric(songId) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/lyric/get/lyric", {
-          id: songId,
-          sig: this.hashParam("/api/v2/lyric/get/lyric", songId),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/lyric/get/lyric", {
+        id: songId,
+        sig: this.hashParam("/api/v2/lyric/get/lyric", songId),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // search
   search(name) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/search/multi", {
-          q: name,
-          sig: this.hashParamNoId("/api/v2/search/multi"),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/search/multi", {
+        q: name,
+        sig: this.hashParamNoId("/api/v2/search/multi"),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getListMV
   getListMV(id, page, count) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/video/get/list", {
-          id: id,
-          type: "genre",
-          page: page,
-          count: count,
-          sort: "listen",
-          sig: this.hashListMV(
-            "/api/v2/video/get/list",
-            id,
-            "genre",
-            page,
-            count
-          ),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/video/get/list", {
+        id: id,
+        type: "genre",
+        page: page,
+        count: count,
+        sort: "listen",
+        sig: this.hashListMV(
+          "/api/v2/video/get/list",
+          id,
+          "genre",
+          page,
+          count
+        ),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getCategoryMV
   getCategoryMV(id) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/genre/get/info", {
-          id: id,
-          type: "video",
-          sig: this.hashCategoryMV("/api/v2/genre/get/info", id, "video"),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/genre/get/info", {
+        id: id,
+        type: "video",
+        sig: this.hashCategoryMV("/api/v2/genre/get/info", id, "video"),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 
   // getVideo
   getVideo(videoId) {
-    return (
-      new Promise() <
-      any >
-      ((resolve, rejects) => {
-        this.requestZingMp3("/api/v2/page/get/video", {
-          id: videoId,
-          sig: this.hashParam("/api/v2/page/get/video", videoId),
-        })
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            rejects(err);
-          });
+    return new Promise((resolve, rejects) => {
+      this.requestZingMp3("/api/v2/page/get/video", {
+        id: videoId,
+        sig: this.hashParam("/api/v2/page/get/video", videoId),
       })
-    );
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          rejects(err);
+        });
+    });
   }
 } // END
 
